d696f72f5cf2b761259683b21ae07567a9f0195e
hisagar <32439113+Hisagar@users.noreply.github.com>
2021-08-31 13:03:26 +0530

    Initial commit

---
+++ Variable_Exercise/make_it_blue.go
@@ -0,0 +1,20 @@
+package main
+
+// ---------------------------------------------------------
+// EXERCISE: Make It Blue
+//
+//  1. Change `color` variable's value to "blue"
+//
+//  2. Print it
+//
+// EXPECTED OUTPUT
+//  blue
+// ---------------------------------------------------------
+import "fmt"
+func main() {
+	// UNCOMMENT THE CODE BELOW:
+
+	color := "green"
+	color = "blue"
+	fmt.Println(color)
+}
